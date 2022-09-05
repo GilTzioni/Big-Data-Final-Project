9e3ac6c5ef55bf0ef13ae109e78c471ec48df034
Gil4747 <gilqwer1@gmail.com>
2022-09-05 15:02:47 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const Port = process.env.PORT || 3000;
+var server = require('http').createServer(app);
+
+//--------------Middleware------------------
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+app.use('/', controllerRouter);
+server.listen(Port, () => console.log(`Server B is listening at http://localhost:${Port}`));
+
