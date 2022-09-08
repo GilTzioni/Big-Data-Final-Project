bbaedb0b0dbf39225e481714de314c5cfeb4d578
Gil4747 <gilqwer1@gmail.com>
2022-09-08 19:15:06 +0300

    init flight

--- models/redisDB.js
+++ models/redisDB.js
@@ -1,29 +1,38 @@
 const Redis = require('redis');
 const db = require('./connectRedis');
-const keys = ["landings", "flights","weather","locations"];
+const keys = ["flight"];
 
 const redisDB = {
 
     //Init all keys
-    initDB: async function() {
+    initDB: async function() 
+    {
         keys.forEach(key => {
             db.set(key, 0);
         });
         console.log('initDB');
     },
 
+    setTopic: async function(topic, value) 
+    {
+        // we can refactor this
+        switch(topic) 
+        {
+            case 'flight':
+                await this.setWaiting('flight', value);
+                break;
+            default:
+                console.log('invalid topic');
+                break;
+        }
+    },
+
     
 
-    getAllData: async function() {
+    getAllData: async function() 
+    {
         let allData = [];
-
-        allData.push(await db.get('landings'));
-        allData.push(await db.get('flights'));
-        allData.push(await db.get('locations'));
-        allData.push(await db.get('leave'));
-        allData.push(await db.get('weather'));
-
-       
+        allData.push(await db.get('flight'));
         console.log("Get all data from Redis!");
         return allData;
     },
